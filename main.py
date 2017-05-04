970ffac4242cc15f3dcf38c5c44b5e378a73f950
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-05-04 18:21:33 +0200

    add docstrings where needed in main and product files.

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ app.secret_key = "secret_key"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    """ show list of products on the page """
     products = Product.get_all()   
     return render_template("index.html", products=products)
 
 @app.route("/product/new", methods=["GET", "POST"])
 def add_product():
+    """ Receive information from the user about new product
+        call methods in product in order to add new product to database """
     if request.method == "GET":
         return render_template("add.html")
     if request.method == "POST":
@@ -28,6 +31,8 @@ def add_product():
         return render_template("add.html")
 
 def validation(name, description, price):
+    """ validate data the user has entered.
+        Return True only in case all data is valid """
     if name == "":
         flash('Name is empty')
         return False
@@ -35,7 +40,7 @@ def validation(name, description, price):
         flash('Description is empty')
         return False
     if price == "":
-        flash('Price is empty')        
+        flash('Price is empty')
         return False
     if not price.isdigit():
         flash('Invalid price')
@@ -49,14 +54,11 @@ def validation(name, description, price):
 
 @app.route("/remove/<product_id>", methods=["GET", "POST"])
 def remove_product(product_id):
+    """ remove product from the list of products """
     my_product = Product.get_by_id(product_id)
-    print(my_product)
     if my_product:
-        print("my_product is not False")
         my_product.remove_product()
     return redirect(url_for("index"))
-    
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- model/product.py
+++ model/product.py
@@ -1,8 +1,9 @@
 from sql_connect import SQL
 
 class Product:
-    """ product class, which represents each product described in database """
+    """ product class, which represents each product described in database. """
     def __init__(self, id, name, description, price):
+        """ class constructor"""
         self.id = id
         self.name = name
         self.description = description
@@ -10,6 +11,7 @@ class Product:
 
     @staticmethod
     def get_all():
+        """ retreive all data from the database. Return list of product instances. """
         product_list = []
         query = "SELECT * FROM products;"
         products = SQL.execute_query(query)
@@ -21,12 +23,15 @@ class Product:
         return product_list
 
     def add_product(self):
+        """ send query and parameters to sql execution method 
+            in order to add new product to database. """
         query = "INSERT INTO products (name, description, price) VALUES(?, ?, ?);"
         params = [self.name, self.description, self.price]
         SQL.execute_query(query, params)
 
     @staticmethod
     def get_by_id(product_id):
+        """ get product by it's id number. """
         products = Product.get_all()
         for product in products:
             if product.id == int(product_id):
@@ -34,6 +39,8 @@ class Product:
         return False
 
     def remove_product(self):
-        query = "DELETE FROM products WHERE ID=?"
+        """ send query and parameters to sql execution method 
+            in order to remove the product from database. """
+        query = "DELETE FROM products WHERE ID=?;"
         params = [self.id]
         SQL.execute_query(query, params)
